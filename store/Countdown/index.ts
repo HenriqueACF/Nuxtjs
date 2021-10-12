9ef7fc1d0377247dbb92f23630dee074a8990faa
henriqueacf <henrique.assis145@gmail.com>
2021-10-12 18:05:12 -0300

    create: store

---
+++ store/Countdown/index.ts
@@ -0,0 +1,30 @@
+// index é responsavel por gerar toda estrutura de uma store do vuex(state, getters, actions, muttations)
+import { State, Getters, Mutations, MutationsInterface } from './types';
+
+const Minutes = 25;
+
+export const state = (): State =>({
+    time: Minutes * 60,
+    isActive: false,
+    hasCompleted: false,
+})
+
+export const getters: Getters = {
+    minutes: state => Math.floor(state.time / 60),
+    seconds: state => state.time % 60
+}
+
+export const mutations: MutationsInterface = {
+    [Mutations.SET_TIME](state, newTime) {
+        state.time = newTime;
+    },
+    [Mutations.RESET_TIME](state) {
+        state.time = Minutes * 60;
+    },
+    [Mutations.SET_IS_ACTIVE](state, isActive) {
+        state.isActive = isActive;
+    },
+    [Mutations.SET_HAS_COMPLETED](state, hasCompleted) {
+        state.hasCompleted = hasCompleted;
+    },
+}
