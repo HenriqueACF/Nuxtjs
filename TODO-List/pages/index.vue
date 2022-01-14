c5982b117731c3d2910142c042d72cd502805825
henriqueacf <henrique.assis145@gmail.com>
2022-01-14 12:06:23 -0300

    up

---
+++ TODO-List/pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <main>
+    <h1>Quadro de tarefas</h1>
+    <p>Crie uma lista de tarefas</p>
+
+    <div class="create-new">
+      <input 
+        type="text"
+        v-model="newTask"
+        placeholder="Adicione uma nova tarefa"
+        @keypress.enter="addTask"
+      />
+      <button @click="addTask">Adicionar</button>
+
+      <div class="tasks">
+        <Task 
+          v-for="(task, i) in $store.state.tasks"
+          :key="i"
+          :task="task"
+        />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data(){
+    return{
+      newTask: ''
+    }
+  },
+  methods:{
+    addTask(){
+      if(this.newTask){
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = ''
+      }
+    }
+  }
+}
+</script>
